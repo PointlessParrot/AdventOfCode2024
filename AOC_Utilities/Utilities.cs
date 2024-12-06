bad97dbc0364eb190cdf756ce566c23e86583cbb
PointlessParrot <jackgent@outlook.com>
2024-12-06 13:51:42 +0000

    AGHHHHHHHHHHH

--- AOC_Utilities/Utilities.cs
+++ AOC_Utilities/Utilities.cs
@@ -1,31 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics.CodeAnalysis;
+// ReSharper disable ConvertToPrimaryConstructor
 
 namespace AOC_Utilities
 {
     public class Grid<T>
     {
-        public int Width;
         public int Height;
-        protected T[,] _data;
+        public int Width;
+        protected T[,] Data;
 
-        public Grid(int width, int height)
+        public Grid(int height, int width)
         {
-            Width = width;
             Height = height;
-            _data = new T[width, height];
+            Width = width;
+            Data = new T[height, width];
+        }
+
+        public Grid(T[,] data)
+        {
+            Height = data.GetLength(0);
+            Width = data.GetLength(1);
+            Data = data;
+        }
+        
+        public T this[int a, int b]
+        {
+            get => Data[a, b];
+            set => Data[a, b] = value;
+        }
+
+        public Grid<TResult> Select<TResult>(Func<T, TResult> selector)
+        {
+            ArgumentNullException.ThrowIfNull(selector);
+            ArgumentNullException.ThrowIfNull(this);
+            
+            Grid<TResult> result = new Grid<TResult>(Height, Width);
+            for (int i = 0; i < Height; i++)
+                for (int j = 0; j < Width; j++)
+                    result[i, j] = selector(Data[i, j]);
+            return result;
         }
     }
 
     public class CharGrid : Grid<char>
     {
-        public CharGrid(int width, int height) : base(width, height)
-        {
-            Width = width;
-            Height = height;
-            _data = new char[width, height];
-        }
+        public CharGrid(int height, int width) : base(height, width) {}
+        public CharGrid(char[,] data) : base(data) {}
+        public CharGrid(string[] lines) : base(lines.Select(x => x.ToCharArray()).ToArray()) {}
+        
     }
     
     internal class Utilities
